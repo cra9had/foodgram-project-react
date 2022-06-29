12b70994c853f9324be81d6eecbb9a3e1b3f32bc
MasterMind7777777 <go.go.nest.nest@gmail.com>
2022-06-29 21:41:25 +0300

    admin add new model

--- backend/foodgram/ingredients/admin.py
+++ backend/foodgram/ingredients/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 from .models import Ingredient
 
 
-# todo
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ("name", "author")
-    list_filter = ('author', 'name', 'tags')
+    list_filter = ('id', 'name', 'unit',)
+    search_fields = ('name',)
