0506783b1b5ce3b6aa2d48202fc5e15cbc8bc68f
MasterMind7777777 <go.go.nest.nest@gmail.com>
2022-07-03 02:56:18 +0300

    userviewset

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import include, re_path
 from rest_framework.routers import DefaultRouter
+from djoser.views import UserViewSet
 
 router = DefaultRouter()
+
+router.register(UserViewSet)
 
 urlpatterns = [
     re_path(r'^', include(router.urls)),
