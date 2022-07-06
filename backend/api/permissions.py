5c2719a69d66e2209d73d3155ddc09c8edf12a37
MasterMind7777777 <go.go.nest.nest@gmail.com>
2022-07-06 20:38:41 +0300

    is_admin rm

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -5,7 +5,6 @@ class UserOrAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.user.is_authenticated
-            and request.user.is_admin
             or request.user.is_staff
         )
 
@@ -21,14 +20,12 @@ class AdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated
-                    and request.user.is_admin
                     or request.user.is_superuser)
                 )
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated
-                    and request.user.is_admin
                     or request.user.is_superuser)
                 )
 
@@ -46,7 +43,6 @@ class AuthorAdminOrReadOnly(permissions.BasePermission):
             or request.user.is_authenticated
             and (
                 request.user.is_staff
-                or request.user.is_admin
                 or request.user.is_superuser
             )
         )
