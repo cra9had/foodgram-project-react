391ff873961a0064a4a050b710e7f79fa20b692b
Khaiam <khaiam.aliev@mail.ru>
2022-07-12 22:53:15 +0300

    fix filters

--- backend/main/views.py
+++ backend/main/views.py
@@ -38,25 +38,16 @@ class RecipeViewSet(viewsets.ModelViewSet):
     # queryset = Recipe.objects.all().order_by('-id')
     permission_classes = (permissions.IsAuthenticated,)
     filter_backends = [DjangoFilterBackend]
-    # filterset_class = RecipeFilter
+    filterset_class = RecipeFilter
     pagination_class = SmallPageNumberPagination
     serializer_class = RecipeSerializer
-
-    # def get_serializer_class(self):
-    #     if self.request.method == 'GET':
-    #         return ReadRecipeSerializer
-    #     return RecipeSerializer
 
     def get_queryset(self):
         is_favorited = self.request.GET.get("is_favorited")
         if not is_favorited:
             return Recipe.objects.all().order_by('-id')
         else:
-            recipes = list()
-            for favorite in Favorite.objects.filter(user=self.request.user):
-                print(favorite.recipe)
-                recipes.append(favorite.recipe)
-            return recipes
+            return Recipe.objects.filter(favouriting__user=self.request.user)
 
     @action(detail=True,
             permission_classes=[permissions.IsAuthenticated],
